1c52fc349780a7ecb96affde18db7d4dd2111506
mohmmedfathi <64088888+mohmmedfathi@users.noreply.github.com>
2023-03-18 03:32:05 +0200

    Committing files

---
+++ CodeForces/GNU C++17/598A | Tricky Sum/126046895.cpp
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(v) (v.begin()),(v.end())
+#define endl "\n"
+#define ll long long
+//v.erase(remove(v.begin(),v.end(),x),v.end()); /*erase-remove idiom.*/
+//v.erase(v.find(x)); --> multiset
+// v[10][10] vector<vector<int>>v5(10,vector<int>(10));
+ 
+int gcd(int a,int b)
+{
+    return b==0?a:gcd(b,a%b);
+}
+bool myCmp (int a,int b)
+{
+    return a>b; /* in sort : descending
+                * set : ascending   */
+}
+int count_ones(int N)
+{
+    int count1 = 0;
+    while (N > 0)
+    {
+        if (N & 1)
+        {
+            count1++;
+        }
+        N = N >> 1;
+    }
+    return count1;
+}
+ 
+int digits (int num)
+{
+    int ans = 0;
+    while(num)
+    {
+        ans++;
+        num/=10;
+    }
+    return ans;
+}
+char s[555];
+void print(int n,int idx)
+{
+    if (n==idx)
+    {
+        cout << s[0] <<s[1] << endl;
+        return;
+    }
+    for (char a='a';a<='z';a++)
+    {
+ 
+        s[idx] = a;
+        print(n,idx+1);
+    }
+}
+long long calc (long long n)
+{
+    return ((n*(n+1))/2);
+}
+int main()
+{
+    vector<long long>v;
+    v.push_back(1);
+    for (int i=1;i<32;i++)
+        v.push_back(v[i-1]*2);
+    int t; cin >> t;
+    while (t--)
+    {
+        long long n; cin >> n;
+        long long ans = calc(n);
+        long long sum = 0;
+        for (int i=0;i<32;i++)
+        {
+            if (v[i]>n) break;
+            else
+                sum+=v[i];
+        }
+        ans -=(sum*2);
+        cout << ans << endl;
+    }
+ 
+}
