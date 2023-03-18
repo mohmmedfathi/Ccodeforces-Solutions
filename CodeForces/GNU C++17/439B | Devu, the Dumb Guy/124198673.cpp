efa5ebadd069003660b0b4ab7978531a83ab8eb3
mohmmedfathi <64088888+mohmmedfathi@users.noreply.github.com>
2023-03-18 03:32:56 +0200

    Committing files

---
+++ CodeForces/GNU C++17/439B | Devu, the Dumb Guy/124198673.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void ScanTeam(int team[], int team_id)
+{
+    int n, person_id;
+    cin >> n;
+    while(n--)
+    {
+        cin >> person_id;
+        team[person_id] = team_id;
+    }
+}
+ 
+int main()
+{
+    long long n,x,arr[100010];
+    long long sum = 0;
+    cin >> n >> x;
+    for (int i=0;i<n;i++)
+        cin >> arr[i];
+    sort(arr,arr+n);
+    for (int i=0;i<n;i++){
+            sum+=arr[i]*x;
+        if(x>1)
+            x--;
+    }
+    cout << sum << endl;
+}
